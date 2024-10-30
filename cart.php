e2aa6130b45d4cedbc8ca93bc6a58f7848326100
Marcelino <marconaoum@gmail.com>
2024-10-30 17:48:42 +0300

    Cart

--- cart.php
+++ cart.php
@@ -1,10 +1,30 @@
 <?php
 
-session_start();
 
-function addcart(){}
-function removecart(){}
-function getcart(){}
+
+function addcart($productid){
+    $quantity = 1;
+    /**
+     * if there is product in cart it add on it
+     */
+    if(isset($_SESSION['cart'][$productid])){
+        $_SESSION['cart'][$productid] += $quantity ;
+
+    }else{
+        $_SESSION['cart'][$productId] = $quantity;
+    }
+
+
+}
+function removecart($productId){ 
+    if (isset($_SESSION['cart'][$productId])) {
+    unset($_SESSION['cart'][$productId]);
+}}
+function getcart(){
+    foreach ($_SESSION['cart'] as $productId => $quantity) {
+        echo "Product ID: $productId, Quantity: $quantity <br>";
+    }
+}
 
 
 
